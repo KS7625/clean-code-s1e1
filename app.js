c6c5aa2765ae2e5b7ddb6a6928fc4f768f6ec7e9
NazarAlexNazar <vitalik.7625@mail.ru>
2021-03-26 15:06:08 +0300

    fix: advanced: 1.3 try to change...

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ var createNewTaskElement=function(taskString){
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
 
-//    deleteButton.innerText="Delete";
+    deleteButton.innerText="Delete";
     deleteButton.className="delete";
     deleteButtonImg.src='./remove.svg';
     deleteButtonImg.alt = "remove";
